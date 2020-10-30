03c0b85543fa02467154b354cbd33f0647a60392
Denis <den_from_bykhov1988@mail.ru>
2020-10-30 22:14:07 +0300

    feat: add solution to carbon-dating and what-season

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,6 +3,12 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  
+module.exports = function dateSample(sampleActivity) {
+  if(typeof sampleActivity !== 'string' || !Number.isInteger(parseInt(sampleActivity, 10)) || sampleActivity > MODERN_ACTIVITY || sampleActivity <= 0) {
+    return false;
+  } else {
+    return Math.ceil((Math.log1p(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD) / 0.693);
+  }
 };
+
+
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
+module.exports = function getSeason(date) {
+  if(arguments.length === 0) {
+    return 'Unable to determine the time of year!';
+  }
+  if(!(arguments[0] instanceof Date)) {
+    throw new Error('Input is invalid');
+  }
+  if (date.hasOwnProperty('toString')) {
+    throw new Error('Input is invalid');
+  }
+  
+  let season;
+  let month = date.getMonth();
+  switch (month) {
+      case 11:
+      case 0:
+      case 1:
+        season = 'winter';      
+        break;
+      case 2:
+      case 3:
+      case 4:
+        season = 'spring';      
+        break;
+      case 5:
+      case 6:
+      case 7:
+        season = 'summer';      
+        break;
+      case 8:
+      case 9:
+      case 10:
+        season = 'autumn';      
+        break;
+    }
+  return season;
   // remove line with error and write your code here
 };
