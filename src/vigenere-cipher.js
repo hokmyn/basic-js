7fc93763b407588ab721fb7e44fe39bc26c9d977
Denis <den_from_bykhov1988@mail.ru>
2020-11-01 20:37:31 +0300

    feat: add solution to vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
+  constructor(mode = true) {
+    this.mode = mode;
+    this.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    this.res = '';
+    this.resIndex;
+  }  
+  encrypt(message, key) {
+    this.res = '';
+    let upperMsg = message.toUpperCase();  
+    let upperKeys = key.toUpperCase();
+    let pos = 0;
+    for (let i = 0; i < upperMsg.length; i++) {
+      if (!this.alphabet.includes(upperMsg[i])) {
+        this.res += upperMsg[i];
+        continue;
+      }
+      this.resIndex = (this.alphabet.indexOf(upperMsg[i]) + this.alphabet.indexOf(upperKeys[pos])) % this.alphabet.length;
+      pos = ++pos % upperKeys.length;
+      this.res += this.alphabet[this.resIndex];
+    }
+    return this.mode ? this.res : this.res.split('').reverse().join('');
     // remove line with error and write your code here
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
+  decrypt(encryptedMsg, key) {
+    this.res = '';
+    let upperMsg = encryptedMsg.toUpperCase();  
+    let upperKeys = key.toUpperCase();
+    let pos = 0;
+    for (let i = 0; i < encryptedMsg.length; i++) {
+      if (!this.alphabet.includes(upperMsg[i])) {
+        this.res += upperMsg[i];
+        continue;
+      }
+      this.resIndex = (this.alphabet.indexOf(encryptedMsg[i]) - this.alphabet.indexOf(upperKeys[pos])) % this.alphabet.length;
+      if(this.resIndex < 0) this.resIndex += this.alphabet.length;
+      pos = ++pos % upperKeys.length;
+      this.res += this.alphabet[this.resIndex];
+    }
+    return this.mode ? this.res : this.res.split('').reverse().join('');    
     // remove line with error and write your code here
   }
 }
